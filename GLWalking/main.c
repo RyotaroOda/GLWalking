f2622ad8195e3a7e7956879229f688251f219e62
RyotaroOda <ryotarooda0619@gmail.com>
2023-06-22 22:10:06 +0900

    second

--- GLWalking/main.c
+++ GLWalking/main.c
@@ -9,7 +9,6 @@
 //https://ja.wikibooks.org/wiki/OpenGL%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0/Glescraft_4
 //https://tokoik.github.io/opengl/libglut.html#12
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
@@ -25,8 +24,8 @@ typedef struct {
 } vec3;
 
 vec3 angles;//視線方向
-vec3 camPos;//カメラ位置
-vec3 lookAt;//
+vec3 camPos = {0, 0.5, 0};//カメラ位置
+vec3 lookAt;
 vec3 center;
 
 vec3 vec3_sub(vec3 a, vec3 b) {
@@ -53,6 +52,7 @@ vec3 vec3_scale(vec3 v, float scalar) {
     return result;
 }
 
+const int groundSize = 50;
 //初期オブジェクトの配置
 void scene(void)
 {
@@ -102,8 +102,8 @@ void scene(void)
     //地面
     glBegin(GL_QUADS);
     glNormal3d(0.0, 1.0, 0.0);
-    for (j = -5; j < 5; ++j) {
-        for (i = -5; i < 5; ++i) {
+    for (j = -groundSize; j < groundSize; ++j) {
+        for (i = -groundSize; i < groundSize; ++i) {
             glMaterialfv(GL_FRONT, GL_DIFFUSE, ground[(i + j) & 1]);
             glVertex3d((GLdouble)i, -0.5, (GLdouble)j);
             glVertex3d((GLdouble)i, -0.5, (GLdouble)(j + 1));
@@ -122,9 +122,16 @@ void display(void){
     center = vec3_add(camPos, lookAt);
     gluLookAt(camPos.x, camPos.y, camPos.z, center.x, center.y, center.z, 0.0, 1.0, 0.0);
     
-    //光源の位置
-    static GLfloat lightpos[] = { 3.0, 4.0, 5.0, 1.0 }; //光源の位置
-    glLightfv(GL_LIGHT0, GL_POSITION, lightpos);
+    //光源
+    static GLfloat lightPosition[4] = { 0.0, 0.0, 10.0, 1.0 }; //光源の位置
+    static GLfloat lightDiffuse[3]  = { 1.0,   1.0, 1.0  }; //拡散光
+    static GLfloat lightAmbient[3]  = { 0.25, 0.25, 0.25 }; //環境光
+    static GLfloat lightSpecular[3] = { 1.0,   1.0, 1.0  }; //鏡面光
+    
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPosition);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  lightDiffuse);
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  lightAmbient);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, lightSpecular);
     
     // シーンの描画
     scene();
@@ -146,43 +153,51 @@ void resize(int w, int h){
     glMatrixMode(GL_MODELVIEW);// モデルビュー変換行列の指定
 }
 
+int mySpecialValue = 0;//移動キー入力
+
 //マウス入力
 void motion(int mx, int my) {
     static bool wrap = false;
-    const float mousespeed = 0.001;//マウス感度調節変数
-    
-    if(!wrap) {
-        int ww = glutGet(GLUT_WINDOW_WIDTH);
-        int wh = glutGet(GLUT_WINDOW_HEIGHT);
-        
-        int dx = mx - ww / 2;//マウスの物理移動量
-        int dy = my - wh / 2;
-        
-        angles.x += dx * mousespeed;
-        angles.y += dy * mousespeed;
-        
-        if(angles.x < -M_PI) angles.x += M_PI * 2;
-        else if(angles.x > M_PI) angles.x -= M_PI * 2;
-        
-        if(angles.y < -M_PI / 2) angles.y = -M_PI / 2;
-        if(angles.y > M_PI / 2) angles.y = M_PI / 2;
-        
-        lookAt.x = sinf(angles.x) * cosf(angles.y);
-        lookAt.y = sinf(angles.y);
-        lookAt.z = cosf(angles.x) * cosf(angles.y);
-        
-        wrap = true;
-        glutWarpPointer(ww / 2, wh / 2);//マウスポインターを中央に戻す
-    } else {
-        wrap = false;
+    const float mousespeed = 0.003;//マウス感度調節変数
+    const bool mouseReverseVertical = true;
+    if (!mySpecialValue) {//移動キーと同時にマウスを動かすとReleaseがうまく反応しないバグを回避
+        if(!wrap) {
+            int ww = glutGet(GLUT_WINDOW_WIDTH);
+            int wh = glutGet(GLUT_WINDOW_HEIGHT);
+            
+            int dx = mx - ww / 2;//マウスの物理移動量
+            int dy = (mouseReverseVertical) ? -(my - wh / 2) : my - wh / 2;
+            
+            angles.x += dx * mousespeed;
+            angles.y += dy * mousespeed;
+            
+            if(angles.x < -M_PI) angles.x += M_PI * 2;
+            else if(angles.x > M_PI) angles.x -= M_PI * 2;
+            
+            if(angles.y < -M_PI / 2) angles.y = -M_PI / 2;
+            if(angles.y > M_PI / 2) angles.y = M_PI / 2;
+            
+            lookAt.x = sinf(angles.x) * cosf(angles.y);
+            lookAt.y = sinf(angles.y);
+            lookAt.z = cosf(angles.x) * cosf(angles.y);
+            
+            wrap = true;
+            glutWarpPointer(ww / 2, wh / 2);//マウスポインターを中央に戻す
+        } else {
+            wrap = false;
+        }
     }
     
     glutPostRedisplay();
 }
 
+const int forward = 1 << 0;//0ビット目を1に
+const int left = 1 << 1;
+const int right = 1 << 2;
+const int backward = 1 << 3;
 
-void keyboard(unsigned char key, int x, int y)
-{
+//キーボードを押した時
+void keyPush(unsigned char key, int x, int y){
     switch (key) {
         case ' '://ジャンプ
             // TODO:
@@ -190,22 +205,50 @@ void keyboard(unsigned char key, int x, int y)
             
         case '\033'://esc
             exit(0);//終了
+            
+        case 'w':
+            mySpecialValue |= forward;
+            break;
+        case 'a':
+            mySpecialValue |= left;
+            break;
+        case 'd':
+            mySpecialValue |= right;
+            break;
+        case 's':
+            mySpecialValue |= backward;
+            break;
+            
+        default:
+            break;
     }
-    
     glutPostRedisplay();
 }
 
-int mySpecialValue = 0;//キーボード入力
+//キーボードを離した時
+void keyRelease(unsigned char key, int x, int y) {
+    switch (key){
+        case 'w':
+            mySpecialValue &= ~(forward);
+            break;
+        case 'a':
+            mySpecialValue &= ~(left);
+            break;
+        case 'd':
+            mySpecialValue &= ~(right);
+            break;
+        case 's':
+            mySpecialValue &= ~(backward);
+            break;
+            
+        default:
+            break;
+    }
+}
 
-const int forward = 1 << 0;//0ビット目を1に
-const int left = 1 << 1;
-const int right = 1 << 2;
-const int backward = 1 << 3;
-
-//特殊キーを押した時
-void keyPush(int key, int x, int y) {
-    switch (key)
-    {
+//特殊キー
+void spKeyPush(int key, int x, int y) {
+    switch (key){
         case GLUT_KEY_UP:
             mySpecialValue |= forward;
             break;
@@ -218,15 +261,14 @@ void keyPush(int key, int x, int y) {
         case GLUT_KEY_DOWN:
             mySpecialValue |= backward;
             break;
+            
         default:
             break;
     }
 }
 
-//特殊キーを離した時
-void keyRelease(int key, int x, int y) {
-    switch (key)
-    {
+void spKeyRelease(int key, int x, int y) {
+    switch (key){
         case GLUT_KEY_UP:
             mySpecialValue &= ~(forward);
             break;
@@ -239,6 +281,7 @@ void keyRelease(int key, int x, int y) {
         case GLUT_KEY_DOWN:
             mySpecialValue &= ~(backward);
             break;
+            
         default:
             break;
     }
@@ -274,14 +317,21 @@ void idle(void) {
     if (mySpecialValue & backward)
         camPos = vec3_sub(camPos, vec3_scale(forward_dir, movespeed * dt));
     
+    //範囲外に移動禁止
+    if(camPos.x > groundSize) camPos.x = groundSize;
+    else if(camPos.x < -groundSize) camPos.x = -groundSize;
+    if(camPos.y > groundSize) camPos.y = groundSize;
+    else if(camPos.y < -groundSize) camPos.y = -groundSize;
+    if(camPos.z > groundSize) camPos.z = groundSize;
+    else if(camPos.z < -groundSize) camPos.z = -groundSize;
+    
     // Redraw the scene
     glutPostRedisplay();
 }
 
-
 //初期化
 void init(void){
-    glClearColor(1.0, 1.0, 1.0, 0.0);
+    glClearColor(135/255.0, 206/255.0, 250/255.0, 0.0);//背景を空色に
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
@@ -294,16 +344,17 @@ int main(int argc, char *argv[]){
     glutInitWindowPosition(100, 100);//ウィンドウ
     glutInitWindowSize(800, 600);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-    glutCreateWindow("windowTitle");
+    glutCreateWindow("windowTitle");//TODO:
     glutDisplayFunc(display);
     glutReshapeFunc(resize);
     
-    glutKeyboardFunc(keyboard);
-    glutSpecialFunc(keyPush);
-    glutSpecialUpFunc(keyRelease);
+    glutKeyboardFunc(keyPush);//キーボード入力
+    glutKeyboardUpFunc(keyRelease);
+    glutSpecialFunc(spKeyPush);
+    glutSpecialUpFunc(spKeyRelease);
     glutIdleFunc(idle);
     
-    glutMotionFunc(motion);//マウスをキャプチャ
+    glutMotionFunc(motion);//マウス入力
     glutPassiveMotionFunc(motion);
     glutSetCursor(GLUT_CURSOR_NONE);//マウスカーソル無効
     
